bcd71bbb26f2e73d43b2698b6e670bab0264cc8b
Diederik van der Boor <vdboor@edoburu.nl>
2013-03-07 11:52:45 +0100

    Cleanup and modernize setup.py and bump version numbers

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+from os.path import dirname, join
 import sys, os
 
 # When creating the sdist, make sure the django.mo file also exists:
@@ -14,17 +15,19 @@ if 'sdist' in sys.argv:
 
 setup(
     name='django-polymorphic-tree',
-    version='0.8.4',
+    version='0.8.5',
     license='Apache License, Version 2.0',
 
     install_requires=[
-        'Django>=1.3.0',
         'django-polymorphic>=0.3.1',
         'django-mptt>=0.5.1',
         'django-tag-parser>=1.0.0',
     ],
+    requires=[
+        'Django (>=1.3)',   # Using staticfiles
+    ],
     description="A polymorphic mptt structure to display content in a tree.",
-    long_description=open('README.rst').read(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
 
     author='Diederik van der Boor',
     author_email='opensource@edoburu.nl',
@@ -39,10 +42,12 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Framework :: Django',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ]
 )
