468279a00166d2e7a5f38b909b64bad444079b2e
Diederik van der Boor <vdboor@edoburu.nl>
2013-03-07 11:50:17 +0100

    Remove utils package as it has become obsolete now.
    The admin+tag parsing utils have been migrated to other apps.
    The JsonResponse turns out to be unused.

--- polymorphic_tree/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The utils package only contains generic features which
-should preferably be merged into separate apps, or Django itself.
-
-They just live in this directory to be easily bundled for now.
-"""
