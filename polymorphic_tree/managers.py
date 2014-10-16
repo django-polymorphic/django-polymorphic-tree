cbbf2e07a891db0cf6aea63dc214bbce096ff03d
Diederik van der Boor <vdboor@edoburu.nl>
2014-10-16 15:59:54 +0200

    Minor bugfix to avoid .toplevel() breaking for RelatedManager code

--- polymorphic_tree/managers.py
+++ polymorphic_tree/managers.py
@@ -1,6 +1,7 @@
 """
 The manager class for the CMS models
 """
+import django
 from mptt.managers import TreeManager
 from polymorphic import PolymorphicManager
 from polymorphic.query import PolymorphicQuerySet
@@ -29,8 +30,9 @@ class PolymorphicMPTTModelManager(TreeManager, PolymorphicManager):
         """
         Return all nodes which have no parent.
         """
-        if hasattr(PolymorphicManager, 'get_queryset'):
+        if hasattr(PolymorphicManager, 'get_queryset') and django.VERSION >= (1,6):
             # Latest django-polymorphic for Django 1.7
+            # Should not be used for Django 1.4/1.5 all all, as that breaks the RelatedManager
             qs = self.get_queryset()
         else:
             qs = self.get_query_set()
