django-polymorphic/django-polymorphic-tree

commit c03595461d687f943b08ce51a215b8ee608a6006

Simplify the get_queryst() / get_query_set() call choice

polymorphic_tree/managers.py
@@ -1,7 +1,6 @@
 """
 The manager class for the CMS models
 """
-import django
 from mptt.managers import TreeManager
 from polymorphic import PolymorphicManager
 from polymorphic.query import PolymorphicQuerySet
@@ -30,11 +29,6 @@ class PolymorphicMPTTModelManager(TreeManager, PolymorphicManager):
         """
         Return all nodes which have no parent.
         """
-        if hasattr(PolymorphicManager, 'get_queryset') and django.VERSION >= (1,6):
-            # Latest django-polymorphic for Django 1.7
-            # Should not be used for Django 1.4/1.5 all all, as that breaks the RelatedManager
-            qs = self.get_queryset()
-        else:
-            qs = self.get_query_set()
-
-        return qs.toplevel()
+        # By using .all(), the proper get_query_set()/get_queryset() will be used for each Django version.
+        # Django 1.4/1.5 need to use get_query_set(), because the RelatedManager overrides that.
+        return qs.all().toplevel()

commit ced52aa8baeea065d62f239cdff6bef128e817c6

Fix the .toplevel() code
Broken by: c03595461d687f943b08ce51a215b8ee608a6006

polymorphic_tree/managers.py
@@ -31,4 +31,4 @@ class PolymorphicMPTTModelManager(TreeManager, PolymorphicManager):
         """
         # By using .all(), the proper get_query_set()/get_queryset() will be used for each Django version.
         # Django 1.4/1.5 need to use get_query_set(), because the RelatedManager overrides that.
-        return qs.all().toplevel()
+        return self.all().toplevel()