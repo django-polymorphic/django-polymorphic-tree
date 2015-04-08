4fc5d99c5cc699497ddaf6ebaeb17fa22b3ffff3
Diederik van der Boor <vdboor@edoburu.nl>
2015-04-08 15:15:39 +0200

    Remove old version checking code in __init__

--- polymorphic_tree/__init__.py
+++ polymorphic_tree/__init__.py
@@ -1,12 +1,2 @@
 # following PEP 386
 __version__ = "1.0.1"
-
-# Do some sane version checking
-import django
-import mptt
-
-if django.VERSION < (1,3,0):
-    raise ImportError("At least Django 1.3.0 is required to run this application")
-
-if mptt.VERSION < (0,5,1):
-    raise ImportError("At least django-mptt 0.5.1 is required to run this application")
