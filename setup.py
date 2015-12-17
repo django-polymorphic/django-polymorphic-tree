bbd5dacaa0513435a663ea80273a96f5f9a805ca
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-17 11:24:56 +0100

    Improved compilemessages logic for setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,10 @@ if 'sdist' in sys.argv or 'develop' in sys.argv:
     os.chdir('polymorphic_tree')
     try:
         from django.core import management
-        management.call_command('compilemessages')
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if 'sdist' in sys.argv:
+            raise
     finally:
         os.chdir('..')
 
