9098a07fc6c7b931e3aa8d41f44e2892ba25aa4e
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-22 15:35:42 +0100

    fix admin include urls

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
@@ -6,5 +6,5 @@ urlpatterns = [
     # url(r'^$', 'example.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
